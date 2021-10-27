3e66caadfd3c92f879328445fa25bc4cb2076c5a
frontendprof <https://github.com/frontendprof>
2021-10-28 03:06:04 +0500

    Setting dynamic content with data/products.json

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,7 +3,7 @@ import s from './Container.module.scss';
 const Container = ({ children, className, ...rest }) => {
   let containerClassname = s.container;
   if (className) {
-    containerClassname = `${containerClassname}${className}`;
+    containerClassname = `${containerClassname} ${className}`;
   }
   return (
     <div className={containerClassname} {...rest}>
